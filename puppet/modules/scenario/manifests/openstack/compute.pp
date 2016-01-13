b3aa3741f11d008cc5f6d397dcffd3168e0e16f1
David Guyon <david@guyon.me>
2016-01-13 08:56:37 +0100

    Add: support for external storage

--- puppet/modules/scenario/manifests/openstack/compute.pp
+++ puppet/modules/scenario/manifests/openstack/compute.pp
@@ -9,6 +9,32 @@ class scenario::openstack::compute (
   String $data_network              = $scenario::openstack::params::network
 ) inherits scenario::openstack::params {
 
+  file {
+    '/tmp/nova':
+      ensure  => directory;
+    ['/tmp/nova/images', '/tmp/nova/instances']:
+      ensure  => directory,
+      owner   => nova,
+      group   => nova,
+      require => File['/tmp/nova'];
+  }
+
+  mount {
+    '/var/lib/nova/instances':
+      ensure  => mounted,
+      device  => '/tmp/nova/instances',
+      fstype  => 'none',
+      options => 'rw,bind';
+    '/var/lib/nova/images':
+      ensure  => mounted,
+      device  => '/tmp/nova/images',
+      fstype  => 'none',
+      options => 'rw,bind',
+  }
+
+  Package['nova-common'] -> File['/tmp/nova/images']    -> Mount['/var/lib/nova/images']
+  Package['nova-common'] -> File['/tmp/nova/instances'] -> Mount['/var/lib/nova/instances']
+
   # common config between controller and computes
   class { '::scenario::common::nova': 
     controller_public_address => $controller_public_address,
