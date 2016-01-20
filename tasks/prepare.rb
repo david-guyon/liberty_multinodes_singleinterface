f17cc1f94f69c3920337db642dae2bed69c5a5f9
David Guyon <david@guyon.me>
2016-01-20 17:03:18 +0100

    Add: task to prepare software on controller node

---
+++ tasks/prepare.rb
@@ -0,0 +1,15 @@
+
+namespace :prepare do
+
+  desc 'Prepare software on controller'
+  task :controller do
+    on(roles('controller'), user: 'root', environment: XP5K::Config[:openstack_env]) do
+      cmd = []
+      cmd << 'apt-get install --yes git'
+      cmd << 'git clone https://davidguyon@bitbucket.org/davidguyon/greenerbar.git'
+      cmd << 'cd greenerbar'
+      cmd << 'bash install-greenerbar.bash'
+      cmd
+    end
+  end
+end
