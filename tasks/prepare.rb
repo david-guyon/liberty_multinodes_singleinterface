ec6c80c9c2d845c72ef9136ce590598cf5983782
David Guyon <david@guyon.me>
2016-02-11 11:33:58 +0100

    Add: cloud ssh-key generation on controller

--- tasks/prepare.rb
+++ tasks/prepare.rb
@@ -5,6 +5,8 @@ namespace :prepare do
   task :controller do
     on(roles('controller'), user: 'root', environment: XP5K::Config[:openstack_env]) do
       cmd = []
+      cmd << 'ssh-keygen -t rsa -f cloud.key -N ""'
+      cmd << 'nova keypair-add --pub_key ~/cloud.key.pub cloud_key'
       cmd << 'apt-get install --yes git'
       cmd << 'git clone https://davidguyon@bitbucket.org/davidguyon/greenerbar.git'
       cmd << 'cd greenerbar ; bash install-greenerbar.bash'
