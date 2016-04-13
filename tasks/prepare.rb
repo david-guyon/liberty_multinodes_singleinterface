2c95fac4c04525ba917f9da42313dda7dbc8b6a1
David Guyon <david@guyon.me>
2016-04-13 11:42:05 +0200

    Change: using isolated-vm branch instead of nofrieda one

--- tasks/prepare.rb
+++ tasks/prepare.rb
@@ -58,7 +58,7 @@ namespace :prepare do
       on(roles('controller'), user: 'root', environment: XP5K::Config[:openstack_env]) do
         cmd = []
         cmd << 'apt-get install --yes git'
-        cmd << 'git clone -b nofrieda https://davidguyon@bitbucket.org/davidguyon/greenerbar.git'
+        cmd << 'git clone -b isolated-vm https://davidguyon@bitbucket.org/davidguyon/greenerbar.git'
         cmd << 'cd greenerbar ; bash install-greenerbar.bash'
         cmd
       end
